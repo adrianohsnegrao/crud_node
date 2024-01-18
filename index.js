39902761cab68f6d6f1639fbda509133c1ee63bc
Adriano Negrão <adriano.negrao@watch.tv.br>
2024-01-17 20:21:07 -0400

    implementando a chamada das rotas

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const produtoRoutes = require('./routes/produtos');
 
 app.use(express.json());
+
+app.use('/api', produtoRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor rodando na porta 3000');
