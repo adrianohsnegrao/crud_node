79760dd245a505304b89be8ff907f440e1fd88ad
Adriano Negrão <adriano.negrao@watch.tv.br>
2024-01-17 20:09:50 -0400

    Criando model produto

---
+++ models/produto.js
@@ -0,0 +1,9 @@
+class Produto {
+    constructor(nome, quantidade, preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+}
+
+module.exports = Produto;
